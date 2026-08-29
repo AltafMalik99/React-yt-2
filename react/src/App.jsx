78a2286884fcb086d6c0000fd8f4d5fd472714e5
Muhammad Altaf <zmaltaf99@gmail.com>
2026-08-30 00:27:33 +0500

    react folder structure

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+
+const App = () => {
+  return (
+   <>
+   
+   <div id='dad'>
+    <h1 id="child1">hello</h1>
+    <h1 id="child2">hello</h1>
+   </div>
+<div id="chacha"></div>
+   </>
+  )
+}
+
+export default App
