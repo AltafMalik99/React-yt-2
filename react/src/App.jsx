929d4ea0d03e1dc2c1e5f905df48813167b3dd1d
Muhammad Altaf <zmaltaf99@gmail.com>
2026-09-02 00:22:22 +0500

    refactor: update App component structure and add Card component

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,15 +1,18 @@
+import React from 'react'
+import Card from './components/card'
 
 const App = () => {
   return (
-   <>
-   
-   <div id='dad'>
-    <h1 id="child1">hello</h1>
-    <h1 id="child2">hello</h1>
-   </div>
-<div id="chacha"></div>
-   </>
+    <div>
+      
+
+<Card user="malik" age={20} img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV6j80x_wUI21m4TY9OsLKrBA-5m5rAZ3S49ITuvQ0Tg&s" />
+<Card user="sad bhai" age={28} img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXz_uXJ3pWNzndxv4HtU9QZroaxEyo1AkC1iMoUHe6Uw&s=10"/>
+
+    </div>
   )
 }
 
 export default App
+
+video 1:47
---
+++ react/src/components/card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const card = (props) => {
+    console.log(props);
+    
+  return (
+    <div className='card'>
+<div>
+
+<img src={props.img} alt="" />
+<h1>{props.user},{props.age}</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, labore?</p>
+
+</div>
+
+    </div>
+  )
+}
+
+export default card
