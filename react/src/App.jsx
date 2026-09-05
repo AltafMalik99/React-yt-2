5b4d2191a932bf24d458a8e30b05e3aa52baf612
Muhammad Altaf <zmaltaf99@gmail.com>
2026-09-05 11:39:19 +0500

    style: update card component styles and layout for improved UI

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -24,26 +24,31 @@ const App = () => {
     <div className='parent'>
 
 <div className="card">
-  <div className="top">
+
+<div>
+    <div className="top">
     <img src="https://techdocs.akamai.com/identity-cloud/img/social-login/identity-providers/amazon-logo.png" alt="" />
-    <button>save <Bookmark /> </button>
+    <button>save <Bookmark size={12}/> </button>
+
+  
   </div>
 <div className="center">
   <h1>Amazon <span>5 day ago</span></h1>
   <h2>Senior UI/UX Designer</h2>
-<div>
+<div className='tag'>
   <h4>part time</h4>
   <h4>senior level</h4>
 
 </div>
 
 </div>
+</div>
 <div className="bottom">
   <div>
       <h3>$120/hr</h3>
-      <p>karachi pakistan</p>
+      <p>karachi pakistan</p> 
     </div>
-    <button>Apply Now</button>
+    <button className='btn'>Apply Now</button>
 
 </div>
 
