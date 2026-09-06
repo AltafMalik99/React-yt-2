066119b82e172cdf70852d0ce3ce33d251556507
Muhammad Altaf <zmaltaf99@gmail.com>
2026-09-07 00:02:35 +0500

    feat: implement User component and update App layout with card structure

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -16,48 +16,16 @@
 // export default App
 
 
-import React from 'react'
-import {Bookmark} from 'lucide-react'
+import Card from './components/card' 
+import User from './components/User'
 
 const App = () => {
   return (
-    <div className='parent'>
-
-<div className="card">
-
-<div>
-    <div className="top">
-    <img src="https://techdocs.akamai.com/identity-cloud/img/social-login/identity-providers/amazon-logo.png" alt="" />
-    <button>save <Bookmark size={12}/> </button>
-
-  
-  </div>
-<div className="center">
-  <h1>Amazon <span>5 day ago</span></h1>
-  <h2>Senior UI/UX Designer</h2>
-<div className='tag'>
-  <h4>part time</h4>
-  <h4>senior level</h4>
-
-</div>
-
-</div>
-</div>
-<div className="bottom">
-  <div>
-      <h3>$120/hr</h3>
-      <p>karachi pakistan</p> 
-    </div>
-    <button className='btn'>Apply Now</button>
-
-</div>
-
-</div>
-
+    <div className="parent">
+      <User />
     </div>
   )
 }
 
 export default App
-
-// videp :2:00
+// videp :2:16
--- react/src/components/card.jsx
+++ react/src/components/card.jsx
@@ -20,3 +20,45 @@
 // export default card
 
 
+
+import {Bookmark} from 'lucide-react'
+
+
+const card = () => {
+  return (
+
+<div className="card">
+
+<div>
+    <div className="top">
+    <img src="https://techdocs.akamai.com/identity-cloud/img/social-login/identity-providers/amazon-logo.png" alt="" />
+    <button>save <Bookmark size={12}/> </button>
+
+  
+  </div>
+<div className="center">
+  <h1>Amazon <span>5 day ago</span></h1>
+  <h2>Senior UI/UX Designer</h2>
+<div className='tag'>
+  <h4>part time</h4>
+  <h4>senior level</h4>
+
+</div>
+
+</div>
+</div>
+<div className="bottom">
+  <div>
+      <h3>$120/hr</h3>
+      <p>karachi pakistan</p> 
+    </div>
+    <button className='btn'>Apply Now</button>
+
+</div>
+
+</div>
+
+  )
+}
+
+export default card
